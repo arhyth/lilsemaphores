caf659c815fbe0497b90a4dbf19c53565e5612c8
David Ebreo <arhyth@gmail.com>
2020-04-18 14:38:01 +0800

    Update package doc

--- doc.go
+++ doc.go
@@ -1,3 +1,6 @@
-// Package lilsem implements common synchronization patterns using semaphore
-// solutions to problems/exercises from The Little Book of Semaphores
+// Package lilsem is a collection of solutions for exercises from the book
+// The Little Book of Semaphores to build higher level concurrency and
+// coordination patterns.
+//
+// This particular repo uses the golang.org/x/sync/semaphore implementation.
 package lilsem
