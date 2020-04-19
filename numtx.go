a192209a0a4a26ca7a2c8c50dd48843616b5e0bf
David Ebreo <arhyth@gmail.com>
2020-04-19 15:11:01 +0800

    Use RWMutex for NuMtx guard

--- numtx.go
+++ numtx.go
@@ -7,16 +7,36 @@ import (
 // NuMtx is a counter guarded by a mutex for testing purposes
 type NuMtx struct {
 	counter int
-	mtx     *sync.Mutex
+	mtx     *sync.RWMutex
 }
 
 // Inc increments counter safely
 func (n *NuMtx) Inc() int {
+	return n.add(1)
+}
+
+// Dec decrements counter safely
+func (n *NuMtx) Dec() int {
+	return n.add(-1)
+}
+
+// Val returns current counter value
+func (n *NuMtx) Val() int {
+	var val int
+
+	n.mtx.RLock()
+	val = n.counter
+	n.mtx.RUnlock()
+
+	return val
+}
+
+func (n *NuMtx) add(a int) int {
 	var prev int
 
 	n.mtx.Lock()
 	prev = n.counter
-	n.counter = n.counter + 1
+	n.counter = n.counter + a
 	n.mtx.Unlock()
 
 	return prev
@@ -25,6 +45,6 @@ func (n *NuMtx) Inc() int {
 // NewNuMtx ...
 func NewNuMtx() *NuMtx {
 	return &NuMtx{
-		mtx: &sync.Mutex{},
+		mtx: &sync.RWMutex{},
 	}
 }
